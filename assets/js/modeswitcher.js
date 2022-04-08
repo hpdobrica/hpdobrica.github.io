e78ab02160181641b068bd1ee22cd0ceea2038f7
hpdobrica <dobrica@getpenta.com>
2022-04-08 13:54:23 +0200

    dark by default

--- assets/js/modeswitcher.js
+++ assets/js/modeswitcher.js
@@ -4,61 +4,60 @@ https://github.com/derekkedziora/jekyll-demo
 Creative Commons Attribution 4.0 International License
 */
 
-let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)"); 
-let theme = sessionStorage.getItem('theme');
+let systemInitiatedDark = window.matchMedia('(prefers-color-scheme: dark)')
+let theme = sessionStorage.getItem('theme') || 'dark'
 
 function changeIconImgSrc(src) {
-	document.getElementById("theme-toggle-img").src = src;
-	document.getElementById("theme-toggle-img--mobile").src = src;
+  document.getElementById('theme-toggle-img').src = src
+  document.getElementById('theme-toggle-img--mobile').src = src
 }
 
 if (systemInitiatedDark.matches) {
-	changeIconImgSrc("../assets/img/moon.svg");
+  changeIconImgSrc('../assets/img/moon.svg')
 } else {
-	changeIconImgSrc("../assets/img/sun.svg");
+  changeIconImgSrc('../assets/img/sun.svg')
 }
 
 function prefersColorTest(systemInitiatedDark) {
   if (systemInitiatedDark.matches) {
-  	document.documentElement.setAttribute('data-theme', 'dark');		
-   	changeIconImgSrc("../assets/img/moon.svg");
-   	sessionStorage.setItem('theme', '');
+    document.documentElement.setAttribute('data-theme', 'dark')
+    changeIconImgSrc('../assets/img/moon.svg')
+    sessionStorage.setItem('theme', '')
   } else {
-  	document.documentElement.setAttribute('data-theme', 'light');
-    changeIconImgSrc("../assets/img/sun.svg");
-    sessionStorage.setItem('theme', '');
+    document.documentElement.setAttribute('data-theme', 'light')
+    changeIconImgSrc('../assets/img/sun.svg')
+    sessionStorage.setItem('theme', '')
   }
 }
-systemInitiatedDark.addListener(prefersColorTest);
-
+systemInitiatedDark.addListener(prefersColorTest)
 
 function modeSwitcher() {
-	let theme = sessionStorage.getItem('theme');
-	if (theme === "dark") {
-		document.documentElement.setAttribute('data-theme', 'light');
-		sessionStorage.setItem('theme', 'light');
-		changeIconImgSrc("../assets/img/sun.svg");
-	}	else if (theme === "light") {
-		document.documentElement.setAttribute('data-theme', 'dark');
-		sessionStorage.setItem('theme', 'dark');
-		changeIconImgSrc("../assets/img/moon.svg");
-	} else if (systemInitiatedDark.matches) {	
-		document.documentElement.setAttribute('data-theme', 'light');
-		sessionStorage.setItem('theme', 'light');
-		changeIconImgSrc("../assets/img/sun.svg");
-	} else {
-		document.documentElement.setAttribute('data-theme', 'dark');
-		sessionStorage.setItem('theme', 'dark');
-		changeIconImgSrc("../assets/img/moon.svg");
-	}
+  let theme = sessionStorage.getItem('theme')
+  if (theme === 'dark') {
+    document.documentElement.setAttribute('data-theme', 'light')
+    sessionStorage.setItem('theme', 'light')
+    changeIconImgSrc('../assets/img/sun.svg')
+  } else if (theme === 'light') {
+    document.documentElement.setAttribute('data-theme', 'dark')
+    sessionStorage.setItem('theme', 'dark')
+    changeIconImgSrc('../assets/img/moon.svg')
+  } else if (systemInitiatedDark.matches) {
+    document.documentElement.setAttribute('data-theme', 'light')
+    sessionStorage.setItem('theme', 'light')
+    changeIconImgSrc('../assets/img/sun.svg')
+  } else {
+    document.documentElement.setAttribute('data-theme', 'dark')
+    sessionStorage.setItem('theme', 'dark')
+    changeIconImgSrc('../assets/img/moon.svg')
+  }
 }
 
-if (theme === "dark") {
-	document.documentElement.setAttribute('data-theme', 'dark');
-	sessionStorage.setItem('theme', 'dark');
-	changeIconImgSrc("../assets/img/moon.svg");
-} else if (theme === "light") {
-	document.documentElement.setAttribute('data-theme', 'light');
-	sessionStorage.setItem('theme', 'light');
-	changeIconImgSrc("../assets/img/sun.svg");
+if (theme === 'dark') {
+  document.documentElement.setAttribute('data-theme', 'dark')
+  sessionStorage.setItem('theme', 'dark')
+  changeIconImgSrc('../assets/img/moon.svg')
+} else if (theme === 'light') {
+  document.documentElement.setAttribute('data-theme', 'light')
+  sessionStorage.setItem('theme', 'light')
+  changeIconImgSrc('../assets/img/sun.svg')
 }
